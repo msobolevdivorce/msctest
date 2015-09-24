a11fb4ea1e28926cf27956a8b266891fccce2589
tenonic <tenonic@gmail.com>
2015-09-24 15:26:26 -0400

    more tests

--- test/calculator.spec.js
+++ test/calculator.spec.js
@@ -13,20 +13,28 @@
 // });
 
 
-describe('calcservice', function(){
+describe('calcservice', function() {
 
-    beforeEach(module('mscApp'));
-    
-    var calcservice;
-	
-    beforeEach(inject(function(_calculatorService_){
-        // The injector unwraps the underscores (_) from around the parameter names when matching
-        calcservice = _calculatorService_;
-    }));
-	
-	it('gets age of Majority for ON', function(){
-		expect(calcservice.ageOfMajority('ON')).toEqual(18);
-	});
-	
+  beforeEach(module('mscApp'));
+
+  var calcservice;
+
+  beforeEach(inject(function(_calculatorService_) {
+    // The injector unwraps the underscores (_) from around the parameter names when matching
+    calcservice = _calculatorService_;
+  }));
+
+  it('gets age of Majority for ON', function() {
+    expect(calcservice.ageOfMajority('ON')).toEqual(18);
+  });
+
+  it('tests if there is a child over age of majority', function() {
+    expect(calcservice.thereIsChildOverAOM('ON',
+      [
+        {name: "Pebbles", age: "8 years", livesWith: "Wilma"},
+        {name: "Pebbles", age: "28 years", livesWith: "Fred"},
+    ])).toBe(true);
+  });
+
+
 });
-	
